af650580ade4075d9d2ea0cf7cc5d825cabc5399
vishaldn <49681201+vishaldn@users.noreply.github.com>
2020-05-10 14:28:21 +0530

    First File For R base

---
+++ R_Overview.R
@@ -0,0 +1,40 @@
+
+
+#### CHAPTER 1 - OVERVIEW ####
+
+
+# Note: You can write a comment in the script by starting a line with the "#" sign
+# A comment is not run by R when you press ctrl + Enter
+
+
+### Assignments
+
+x = 1
+
+y <- 2
+
+
+### functions
+
+print("Hello World")
+
+
+
+### Computations
+
+17 + 5
+
+x + 5
+
+y = 15 +5
+
+y = sqrt(16) # Square Root function
+
+
+sqrt(16)
+
+# Call y, see y value in console
+y
+
+# Call x, see x value in console
+x
